731e13d856ba39e81be0e8b6f18fab3ea4a2d9ab
Edwin Bosire <me@edwinb.co.uk>
2020-09-19 15:34:39 +0100

    removed stretch_header dependancy and used Slivers instead

--- lib/widgets/menu_page.dart
+++ lib/widgets/menu_page.dart
@@ -3,7 +3,6 @@ import 'package:life_in_the_uk/screens/QuizPage.dart';
 import 'package:life_in_the_uk/utilities/constants.dart';
 import 'package:life_in_the_uk/viewModel/exam_view_model.dart';
 import 'package:provider/provider.dart';
-import 'package:stretchy_header/stretchy_header.dart';
 
 import 'HeaderView.dart';
 import 'MenuListTitle.dart';
@@ -15,50 +14,58 @@ class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StretchyHeader.listView(
-        headerData: HeaderData(
-          headerHeight: kHeaderHeight,
-          header: HeaderView(),
-        ),
-        children: <Widget>[
-          MenuListTile(
-            onPress: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) {
-                  return ChangeNotifierProvider(
-                    create: (_) => ExamViewModel.initWithTestData(),
-                    child: QuizPage(),
-                  );
-                }),
-              );
-            },
-            iconName: 'Mock',
-            title: 'Mock Exam',
-            subTitle: '24 Practise questions to prepare you for the test',
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: kHeaderHeight,
+            flexibleSpace: FlexibleSpaceBar(
+              background: HeaderView(),
+            ),
           ),
-          MenuListTile(
-            onPress: () {},
-            iconName: 'Practice',
-            title: 'Practice Questions',
-            subTitle: '1,000+ Questions to practice',
-          ),
-          MenuListTile(
-            onPress: () {},
-            iconName: 'Revision',
-            title: 'Revision',
-            subTitle: 'All the reading material needed for the test',
-          ),
-          MenuListTile(
-            onPress: () {},
-            iconName: 'Tips',
-            title: 'Tips to pass the test',
-            subTitle: 'Helpful tips to help you pass the exam',
-          ),
-          MenuListTile(
-            onPress: () {},
-            iconName: 'Rate',
-            title: 'Rate Us',
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                MenuListTile(
+                  onPress: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) {
+                        return ChangeNotifierProvider(
+                          create: (_) => ExamViewModel.initWithTestData(),
+                          child: QuizPage(),
+                        );
+                      }),
+                    );
+                  },
+                  iconName: 'Mock',
+                  title: 'Mock Exam',
+                  subTitle: '24 Practise questions to prepare you for the test',
+                ),
+                MenuListTile(
+                  onPress: () {},
+                  iconName: 'Practice',
+                  title: 'Practice Questions',
+                  subTitle: '1,000+ Questions to practice',
+                ),
+                MenuListTile(
+                  onPress: () {},
+                  iconName: 'Revision',
+                  title: 'Revision',
+                  subTitle: 'All the reading material needed for the test',
+                ),
+                MenuListTile(
+                  onPress: () {},
+                  iconName: 'Tips',
+                  title: 'Tips to pass the test',
+                  subTitle: 'Helpful tips to help you pass the exam',
+                ),
+                MenuListTile(
+                  onPress: () {},
+                  iconName: 'Rate',
+                  title: 'Rate Us',
+                ),
+              ],
+            ),
           ),
         ],
       ),
